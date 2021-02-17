f8902695d1b55fe969fcfdd0114c76945f6304b3
Brandon Kim <bkim2490@gmail.com>
2021-02-17 21:46:45 +0900

    dropdown list of individual jobs

--- components/JobItem.js
+++ components/JobItem.js
@@ -1,10 +1,42 @@
+import { useState } from 'react';
+
 const JobItem = ({ job }) => {
+
+	const [showJobs, setShowJobs] = useState(false);
+
+	const toggleJobs = () => {
+		setShowJobs(!showJobs);
+	}
+
+	const jobsList = () => {
+		if (showJobs) {
+			return (
+				<ul className='divide-y'>
+					<li className='mt-4'></li>
+					{job.items.map((item) => (
+						<li className='flex flex-col py-4'>
+							<div>
+								<strong>{item.job_title}</strong>
+							</div>
+							<div>
+								{`${item.job_type} | $${item.salary_range[0]} - $${item.salary_range[1]} an hour | ${item.city}`}
+							</div>
+						</li>
+					))}
+				</ul>
+			)
+		}
+	}
+
 	return (
-		<div className='flex items-center justify-content'>
-			<div className='rounded-lg w-10 flex justify-center bg-gray-300 text-xl p-1 mr-2  text-white'>
-				{job.name.toUpperCase().substring(0, 2)}
+		<div>
+			<div className='flex items-center justify-content' onClick={toggleJobs}>
+				<div className='rounded-lg w-10 flex justify-center bg-gray-300 text-xl p-1 mr-2  text-white'>
+					{job.name.toUpperCase().substring(0, 2)}
+				</div>
+				{`${job.total_jobs_in_hospital} Jobs for ${job.name}`}
 			</div>
-			{`${job.total_jobs_in_hospital} Jobs for ${job.name}`}
+			{jobsList()}
 		</div>
 	)
 }
