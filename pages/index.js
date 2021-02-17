891990f6e6fe961fa8bdd08ec29f7708c940853d
Brandon Kim <bkim2490@gmail.com>
2021-02-17 20:57:43 +0900

    footer template

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,10 @@ import JobList from '../components/JobList';
 const Index = ({ jobs, filters }) => (
 	<>
 		<Nav />
-		<div className='bg-gray-200 mx-auto px-1'>
+		<div className='bg-gray-200 mx-auto px-1 py-4'>
 			<div className="relative mx-4 text-gray-600">
 				<input
-					className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 my-4 rounded-lg text-sm focus:outline-none w-full"
+					className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none w-full"
 					type="search"
 					name="search"
 					placeholder="Search"
@@ -22,6 +22,11 @@ const Index = ({ jobs, filters }) => (
 				<FilterList filters={filters} />
 				<JobList jobs={jobs.jobs} />
 			</div>
+		</div>
+		<div className='flex flex-wrap items-center justify-between px-5 h-48'>
+			<div className='flex'>About us</div>
+			<div className='flex'>Sitemap</div>
+			<div className='flex'>Privacy</div>
 		</div>
 	</>
 )
