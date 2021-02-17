f282e3162ab5138f977c3d5fcaa232059db62f22
Brandon Kim <bkim2490@gmail.com>
2021-02-17 21:24:26 +0900

    2 letter job location

--- components/JobItem.js
+++ components/JobItem.js
@@ -1,9 +1,10 @@
 const JobItem = ({ job }) => {
 	return (
-		<div className='flex relative'>
-			<div>
-				{job.name.toUpperCase().substring(0, 2)} {`${job.total_jobs_in_hospital} Jobs for ${job.name}`}
+		<div className='flex items-center justify-content'>
+			<div className='rounded-lg w-10 flex justify-center bg-gray-300 text-xl p-1 mr-2  text-white'>
+				{job.name.toUpperCase().substring(0, 2)}
 			</div>
+			{`${job.total_jobs_in_hospital} Jobs for ${job.name}`}
 		</div>
 	)
 }
--- components/JobList.js
+++ components/JobList.js
@@ -4,10 +4,10 @@ const JobList = ({ jobs }) => {
 	return (
 		<div className='col-span-2 col-start-2 p-4 bg-white rounded-md items-left'>
 			<div className='flex'>
-				<div className='float-left'>
-					<strong>1423</strong> job postings
+				<div className='flex-1'>
+					<strong>7,753</strong> job postings
 				</div>
-				<div className='float-right'>
+				<div>
 					Sort By
 					Location
 					Role
