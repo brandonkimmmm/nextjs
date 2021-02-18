abe1e90c98bd64ecb1ec514d450edce90c5b67dc
Brandon Kim <bkim2490@gmail.com>
2021-02-18 23:13:29 +0900

    filters list update

--- components/FilterItem.js
+++ components/FilterItem.js
@@ -1,12 +1,32 @@
+import { useState } from 'react';
+
 const FilterItem = ({ name, data }) => {
+
+	const [splitItems, setSplitItems] = useState([data.slice(0, 10), data.slice(10)]);
+	const [showExtra, setShowExtra] = useState(splitItems[1].length > 0 ? false : true);
+
 	return (
-		<div className='bg-white rounded-lg w-full border py-3 px-1'>
-			<h3 className='p-1'>{name}</h3>
-			<ul>
-				{data.map((d) => (
-					<li className='p-1'>{d.key} <span>{d.doc_count}</span></li>
+		<div className='bg-white rounded-lg w-full border p-6'>
+			<strong className='text-lg'>{name}</strong>
+			<ul className='mt-4 space-y-3'>
+				{splitItems[0].map((d) => (
+					<li>
+						{d.key} &nbsp;&nbsp;<span className='text-gray-400 text-sm'>{d.doc_count}</span>
+					</li>
 				))}
+				{splitItems[1].length > 0 && showExtra
+					? splitItems[1].map((d) => (
+						<li>
+							{d.key} &nbsp;&nbsp;<span className='text-gray-400 text-sm'>{d.doc_count}</span>
+						</li>
+					))
+					: null
+				}
 			</ul>
+			{splitItems[1].length > 0
+				? <div className='text-blue-400 mt-4' onClick={() => setShowExtra(!showExtra)}>{showExtra ? 'Show Less' : 'Show More'}</div>
+				: null
+			}
 		</div>
 	)
 }
--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,89 @@
 import { API_URL } from '../constants';
 import Nav from '../components/Nav';
-import FilterList from '../components/FilterList';
-import JobList from '../components/JobList';
+import FilterItem from '../components/FilterItem';
+import JobItem from '../components/JobItem';
+import { useState } from 'react';
+import Dropdown from '../components/Dropdown';
 
-const Index = ({ jobs, filters }) => (
-	<>
-		<Nav />
-		<div className='bg-gray-200 mx-auto px-1 py-4'>
-			<div className="relative mx-4 text-gray-600">
-				<input
-					className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none w-full"
-					type="search"
-					name="search"
-					placeholder="Search"
-				/>
-				<button type="submit" className="absolute right-0 top-0 mt-5 mr-4">
-					<div>Submit</div>
-				</button>
+const Index = ({ jobs, filters }) => {
+	const queryOptions = ['location', 'role', 'department', 'education', 'experience'];
+
+	const [activeQueries, setActiveQueries] = useState({});
+
+	const setQuery = (query, option) => {
+		setActiveQueries({
+			...activeQueries,
+			[query]: option
+		});
+	};
+
+
+	return (
+		<>
+			<Nav />
+			<div className='bg-gray-200 mx-auto px-1 py-4'>
+				<div className="relative mx-4 text-gray-600">
+					<input
+						className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none w-full"
+						type="search"
+						name="search"
+						placeholder="Search"
+					/>
+					<button type="submit" className="absolute right-0 top-0 mt-5 mr-4">
+						<div>Submit</div>
+					</button>
+				</div>
+				<div className='flex flex-row space-x-4 m-4'>
+					<div className='flex flex-col w-1/3 space-y-4 items-center'>
+						{Object.keys(filters).map((filter) => (
+							<FilterItem name={filter.toUpperCase().replace('_', ' ')} data={filters[filter]} />
+						))}
+					</div>
+					<div className='w-2/3 py-6 px-4 flex flex-col bg-white rounded-md'>
+						<div className='flex flex-row pt-8 space-x-4'>
+							<div className='flex-1'>
+								<strong className='text-xl'>7,753</strong> job postings
+							</div>
+							<div className='text-gray-400'>
+								Sort By
+							</div>
+							{queryOptions.map((query) => (
+								<Dropdown query={query} options={['asc', 'desc', 'clear']} onOptionSelect={setQuery} />
+							))}
+						</div>
+						<ul className='pt-10 space-y-8'>
+							{jobs.jobs.map((job) => (
+								<li>
+									<JobItem job={job} />
+								</li>
+							))}
+						</ul>
+					</div>
+				</div>
 			</div>
-			<div className='grid grid-cols-3 gap-4 mx-4 mt-4'>
-				<FilterList filters={filters} />
-				<JobList jobs={jobs.jobs} />
+			<div className='flex flex-row p-6 w-full'>
+				<div className='flex flex-col w-3/5 space-y-2'>
+					<strong className='text-2xl'>About us</strong>
+					<div>We are a team of nurses, doctors, technologists and executives dedicated to help nurses find jobs that they love.</div>
+					<div>All copyrights reserved &copy; 2020 - Health Explore</div>
+				</div>
+				<div className='flex flex-col w-1/5 space-y-2'>
+					<strong className='text-2xl'>Sitemap</strong>
+					<div>Nurses</div>
+					<div>Employers</div>
+					<div>Social networking</div>
+					<div>Jobs</div>
+				</div>
+				<div className='flex flex-col w-1/5 space-y-2'>
+				<strong className='text-2xl'>Privacy</strong>
+					<div>Terms of use</div>
+					<div>Privacy policy</div>
+					<div>Cookie policy</div>
+				</div>
 			</div>
-		</div>
-		<div className='flex flex-row p-6 w-full'>
-			<div className='flex flex-col w-3/5 space-y-2'>
-				<strong className='text-2xl'>About us</strong>
-				<div>We are a team of nurses, doctors, technologists and executives dedicated to help nurses find jobs that they love.</div>
-				<div>All copyrights reserved &copy; 2020 - Health Explore</div>
-			</div>
-			<div className='flex flex-col w-1/5 space-y-2'>
-				<strong className='text-2xl'>Sitemap</strong>
-				<div>Nurses</div>
-				<div>Employers</div>
-				<div>Social networking</div>
-				<div>Jobs</div>
-			</div>
-			<div className='flex flex-col w-1/5 space-y-2'>
-			<strong className='text-2xl'>Privacy</strong>
-				<div>Terms of use</div>
-				<div>Privacy policy</div>
-				<div>Cookie policy</div>
-			</div>
-		</div>
-	</>
-)
+		</>
+	);
+};
 
 export default Index
 
