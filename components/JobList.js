dc261efa3b907d392c2410690196732b53789199
Brandon Kim <bkim2490@gmail.com>
2021-02-18 22:21:21 +0900

    dropdowns finished

--- components/JobList.js
+++ components/JobList.js
@@ -1,30 +1,41 @@
 import JobItem from './JobItem';
+import { useState } from 'react';
+import Dropdown from './Dropdown';
 
 const JobList = ({ jobs }) => {
+
+	const queryOptions = ['location', 'role', 'department', 'education', 'experience'];
+
+	const [activeQueries, setActiveQueries] = useState({});
+
+	const setQuery = (query, option) => {
+		setActiveQueries({
+			...activeQueries,
+			[query]: option
+		});
+	};
+
+
 	return (
-		<div className='col-span-2 col-start-2 p-4 bg-white rounded-md'>
-			<div className='flex'>
+		<div className='col-span-2 col-start-2 py-6 px-4 bg-white rounded-md'>
+			<div className='flex flex-row pt-8 space-x-4'>
 				<div className='flex-1'>
-					<strong>7,753</strong> job postings
+					<strong className='text-xl'>7,753</strong> job postings
 				</div>
-				<div>
+				<div className='text-gray-400'>
 					Sort By
-					Location
-					Role
-					Department
-					Education
-					Experience
 				</div>
+				{queryOptions.map((query) => (
+					<Dropdown query={query} options={['asc', 'desc', 'clear']} onOptionSelect={setQuery} />
+				))}
 			</div>
-			<div className='mt-12'>
-				<ul className='space-y-8'>
-					{jobs.map((job) => (
-						<li>
-							<JobItem job={job} />
-						</li>
-					))}
-				</ul>
-			</div>
+			<ul className='pt-10 space-y-8'>
+				{jobs.map((job) => (
+					<li>
+						<JobItem job={job} />
+					</li>
+				))}
+			</ul>
 		</div>
 	)
 }
