9e8470199d3953675f4e3a7104e675208715c43c
Brandon Kim <bkim2490@gmail.com>
2021-02-21 12:57:57 +0900

    vercel url fix

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
-export const API_URL = process.env.NODE_ENV !== 'production' ? 'http://localhost:3000/api' : 'https://nextjs-9uvqjejl3-brandonkimmmm.vercel.app/api';
+export const API_URL = process.env.NODE_ENV !== 'production' ? 'http://localhost:3000/api' : `${process.env.VERCEL_URL}/api`;
 
