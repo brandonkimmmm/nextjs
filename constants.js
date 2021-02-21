cc05881be414315aa39dae6cbda0df05e9f68bbf
Brandon Kim <bkim2490@gmail.com>
2021-02-21 12:53:08 +0900

    api update

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
-export const API_URL = process.env.NODE_ENV !== 'production' ? 'http://localhost:3000/api' : 'https://vercel.com/brandonkimmmm/nextjs/api';
+export const API_URL = process.env.NODE_ENV !== 'production' ? 'http://localhost:3000/api' : 'https://nextjs-9uvqjejl3-brandonkimmmm.vercel.app/api';
 
